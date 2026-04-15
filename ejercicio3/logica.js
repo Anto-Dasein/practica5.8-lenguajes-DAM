b663520a1cc16ea5882f80ae41ebb92de64ab83c
Anto-Dasein <anto.dasein@gmail.com>
2026-04-15 21:38:27 +0200

    quick fix

--- ejercicio3/logica.js
+++ ejercicio3/logica.js
@@ -1,14 +1,14 @@
 fetch('hardware.json')
-.then(response => response.json)
+.then(response => response.json())
 .then(data => {
     let lista = document.getElementById("lista")
 
     for (let componente of data.hardware) {
 
-        almacenarLi = document.getElementById("li"); // Importante poner el elemento NO su id.
+        almacenarLi = document.createElement("li"); // Importante poner el elemento NO su id.
 
         almacenarLi.innerHTML = componente.marca + " - " + componente.tipo + " - " + componente.precio;
-        
-        (lista).appendChild.almacenarLi;
+
+        lista.appendChild(almacenarLi);
     }
 })
